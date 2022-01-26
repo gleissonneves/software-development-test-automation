1845f18e9c616def924e6808856e0a7c7c00db44
gleissonneves <gleissonneves2017@gmail.com>
2022-01-25 23:47:57 -0300

    feat: create test automation

--- spec/exemplo/exemploSpec.js
+++ spec/exemplo/exemploSpec.js
@@ -2,4 +2,8 @@ describe('Meu primeiro teste com Jasmine e NodeJS', function() {
 	it('deve executar meu primeiro teste', function() {
 		expect(true).toEqual(true)
 	})
+
+	it('deve mostrar que o karma está funcionando', function() {
+		expect(true).toEqual(true)
+	})
 })
